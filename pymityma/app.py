b0577955f6dc556ff1448ecfbd33e92d4251344e
nichollas.pavloski <nichollas.pavloski@vidyatec.com>
2022-11-15 01:49:36 -0300

    add configurable port to run the server

--- pymityma/app.py
+++ pymityma/app.py
@@ -1,7 +1,8 @@
+import os
 from api.server import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    print("starting sever in 8080")
-    app.run(host='172.18.0.1', port=8080, debug=True)
+    print(f'starting sever in {os.getenv("DEFAULT_PORT")}',)
+    app.run(host='172.18.0.1', port=os.getenv("DEFAULT_PORT"), debug=True)
